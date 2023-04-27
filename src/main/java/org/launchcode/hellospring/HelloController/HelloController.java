9552b45d2745570189aa322449bfb72c70ec0c6b
“juniperjer” <115501141+juniperjer@users.noreply.github.com>
2023-04-27 17:26:55 -0500

    lesson 13

--- src/main/java/org/launchcode/hellospring/HelloController/HelloController.java
+++ src/main/java/org/launchcode/hellospring/HelloController/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
-@RequestMapping("hello")
+@RequestMapping(value= "hello",method = {RequestMethod.GET, RequestMethod.POST})
 public class HelloController {
     //for get requests, request to root path
 //    @GetMapping("hello")
@@ -13,14 +13,14 @@ public class HelloController {
 //    public String hello(){
 //        return "Hello Spring!";
 //    }
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name){//@requestparam for including params within the query
-        return "hello "+ name;
-    }
+
+//    public String helloWithQueryParam(@RequestParam String name){//@requestparam for including params within the query
+//        return "hello "+ name;
+//    }
 
     //handles request of the form /hello/LaunchCode (this is part of the path)
     //this is different from the commented out one because it is handling with the {name}
-    @GetMapping("{name}")
+    @GetMapping("hello/{name}")
     public String hellowWithPathParam(@PathVariable String name){
         return "Hello"+ name;
     }
@@ -28,23 +28,7 @@ public class HelloController {
     //now lives at hello/form
     @GetMapping("form")
     public String helloForm(){
-        return "<html>" +
-                "<body>" +
-                "<form action= 'hello' method = 'post'>" + //submit a request to /hello
-                "<input type = 'text' name= 'name'>" +
-                "<input type = 'submit' value ='Greet Me'>" +
-                "</form>" +
-                "</body>" +
-                "<html>";
-    }
-    @GetMapping("form")
-    public String helloSelect(){
-        return "<label for= 'helloSelect'>" +
-                    "<select name= 'language'>" +
-                        "<option value 'Tagalog'>Tagalog</option>" +
-                        "<option value 'English'>English</option>" +
-                    "</select>" +
-                "</label>";
+        return "form";
     }
 
 }
